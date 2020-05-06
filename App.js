b8e164b7b1463be09eab784a1c75682295493fd0
saurabh kacholiya <saurabhkacholiya@Devs-MacBook-Pro-2.local>
2020-05-06 23:36:32 +0530

    add flicker api

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React , { useEffect , useState} from 'react';
+import { 
+  StyleSheet, 
+  Text, 
+  View,
+  Image,
+  ScrollView,
+  SafeAreaView,
+ } from 'react-native';
+import axios from "axios";
+const API_KEY = 'ddfc2122dc4634f03eec5673ea678f8a'
 
 export default function App() {
+  const [response,setResponse] = useState(null)
+  
+  useEffect(() => {
+    const urlEndpoint = `https://api.flickr.com/services/rest/?
+    method=flickr.photos.search&api_key=${API_KEY}&format=json&text=Random
+    &nojsoncallback=true&per_page=20&extras=url_s&page=5`;
+
+    axios.get(urlEndpoint)
+    .then(response => setResponse(response.data.photos.photo))
+    .catch((error) => { console.log(error)})
+
+  },[])
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+        <ScrollView style={{flex:1}}>
+          {
+            response &&
+              response.map((item) => {
+                console.log('item ', item)
+                  return (
+                    <View key={item.id}>
+                        <Image style={{height:100,width:100}} source={{uri: item.url_s}} />
+                    </View>
+                  )
+              })
+          }
+        </ScrollView>
+    </SafeAreaView>
   );
 }
 
