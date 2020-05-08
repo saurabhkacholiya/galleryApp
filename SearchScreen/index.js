06a5c9551805dd3073b147998c49b076e46b4482
saurabh kacholiya <saurabhkacholiya@Devs-MacBook-Pro-2.local>
2020-05-08 08:44:27 +0530

    linting changes

--- SearchScreen/index.js
+++ SearchScreen/index.js
@@ -1,165 +1,169 @@
-import React , { useEffect , useState} from 'react';
-import { 
-  StyleSheet, 
-  View,
-  Image,
-  FlatList,
-  Dimensions,
-  TextInput,
-  TouchableOpacity,
- } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import {
+    StyleSheet,
+    View,
+    Image,
+    FlatList,
+    Dimensions,
+    TextInput,
+    TouchableOpacity,
+} from 'react-native';
 import axios from "axios";
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { 
+import {
     FontAwesome,
     Entypo,
 } from 'react-native-vector-icons';
-import { 
-    getItem, 
-    setItem, 
+import {
+    getItem,
+    setItem,
 } from "./asyncStorage";
 import NetInfo from '@react-native-community/netinfo';
-import { arr  } from "./constant";
+import { arr } from "./constant";
 
 const ITEM_WIDTH = Dimensions.get('window').width;
 const API_KEY = 'ddfc2122dc4634f03eec5673ea678f8a';
 
 
 
-   
-export default function SearchScreen() {
-  const [response, setResponse] = useState([])
-  const [numberOfColumn, setNumberOfColumn] = useState(3)
-  const [searchTerm , setSearchTerm] = useState('Random')
-  const [pageNo, setPageNo] = useState(1)
-  const [refreshing, setRefreshing] = useState(false)
 
-  useEffect(() => {
-    NetInfo.fetch().then(state => {
-        if(state.isConnected){
-            const urlEndpoint = `https://api.flickr.com/services/rest/?
-            method=flickr.photos.search&api_key=${API_KEY}&format=json&text=${searchTerm}
-            &nojsoncallback=true&per_page=20&extras=url_s&page=${pageNo}`;
+export default function SearchScreen() {
+    const [response, setResponse] = useState([])
+    const [numberOfColumn, setNumberOfColumn] = useState(3)
+    const [searchTerm, setSearchTerm] = useState('Random')
+    const [pageNo, setPageNo] = useState(1)
+    const [refreshing, setRefreshing] = useState(false)
+    const [networkStatus, setNetworkStatus] = useState(true)
+
+    useEffect(() => {
+        NetInfo.fetch().then(state => {
+            setNetworkStatus(state.isConnected)
+        })
+    }, [])
     
+    useEffect(() => {
+        if (networkStatus) {
+            const urlEndpoint = `https://api.flickr.com/services/rest/?
+        method=flickr.photos.search&api_key=${API_KEY}&format=json&text=${searchTerm}
+        &nojsoncallback=true&per_page=20&extras=url_s&page=${pageNo}`;
+
             axios.get(urlEndpoint)
                 .then(item => {
-                    setResponse([...response,...item.data.photos.photo])
-                    setItem("searchTerm",JSON.stringify([...response,...item.data.photos.photo]))
+                    setResponse([...response, ...item.data.photos.photo])
+                    setItem("searchTerm", JSON.stringify([...response, ...item.data.photos.photo]))
                 })
-                .catch((error) => { console.log("error => ",error)})
-        }else{
+                .catch((error) => { console.log("error => ", error) })
+        } else {
             getItem("searchTerm")
-                .then(item => setResponse(item) )
+                .then(item => setResponse(item))
                 .catch(error => console.log('error ', error))
         }
-        
+
         setRefreshing(false)
-        
-    })
-  },[searchTerm,pageNo])
+    }, [searchTerm, pageNo, networkStatus])
 
 
 
-const debounce = function (callback, delay) {
-    let timer;
-    return function (data) {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-            callback(data);
-        }, delay);
+    const debounce = function (callback, delay) {
+        let timer;
+        return function (data) {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                callback(data);
+            }, delay);
+        }
     }
-}
 
-const setDataAfterTimeOut = (data) => {
-    setPageNo(1)
-    setResponse([])
-    setSearchTerm(data)
-}
-  
-const onChange = debounce(setDataAfterTimeOut, 300);
-
-const changeColumn = () => {
-    if(numberOfColumn === 4){
-        setNumberOfColumn(2)
-    }else{
-        setNumberOfColumn(numberOfColumn + 1)
+    const setDataAfterTimeOut = (data) => {
+        setPageNo(1)
+        setResponse([])
+        setSearchTerm(data)
     }
-}
 
-const refreshPage = () => {
-    setRefreshing(true)
-    setSearchTerm('happy_days')
-    setPageNo(1)
-}
-  return (
-    <SafeAreaView style={styles.safeAreaViewStyle}>
-        <View style={styles.headerView}>
-            <View style={styles.searchView}>
-                <View style={styles.searchViewWithElement}>
-                    <FontAwesome
-                        style={styles.iconStyle}
-                        name="search"
-                        color={"#F7882F"}
-                        size={16}
-                    />
-                    <TextInput
-                        style={styles.textInputStyle}
-                        placeholder="Search Images"
-                        onChangeText={onChange}
-                    />
+    const onChange = debounce(setDataAfterTimeOut, 300);
+
+    const changeColumn = () => {
+        if (numberOfColumn === 4) {
+            setNumberOfColumn(2)
+        } else {
+            setNumberOfColumn(numberOfColumn + 1)
+        }
+    }
+
+    const refreshPage = () => {
+        setRefreshing(true)
+        setSearchTerm('happy_days')
+        setPageNo(1)
+    }
+    return (
+        <SafeAreaView style={styles.safeAreaViewStyle}>
+            <View style={styles.headerView}>
+                <View style={styles.searchView}>
+                    <View style={styles.searchViewWithElement}>
+                        <FontAwesome
+                            style={styles.iconStyle}
+                            name="search"
+                            color={"#F7882F"}
+                            size={16}
+                        />
+                        <TextInput
+                            style={styles.textInputStyle}
+                            placeholder="Search Images"
+                            onChangeText={onChange}
+                        />
+                    </View>
                 </View>
             </View>
-        </View>
-        <View style={{flex:1,flexDirection: 'row',backgroundColor:'#fff'}}>
-        <FlatList 
-            data={response}
-            renderItem={({item}) => (
-                <View key={`${item.id}_${item.secret}`} style={{margin:5}}>
-                <Image style={{
-                    flex:1,
-                    height:100,
-                    width: (ITEM_WIDTH - 10*numberOfColumn)/numberOfColumn
-                    }} 
-                    source={{uri: item.url_s}} 
-                    resizeMode="contain"
+            <View style={{ flex: 1, flexDirection: 'row', backgroundColor: '#fff' }}>
+                <FlatList
+                    data={response}
+                    renderItem={({ item }) => (
+                        <View key={`${item.id}_${item.secret}`} style={{ margin: 5 }}>
+                            <Image style={{
+                                flex: 1,
+                                height: 100,
+                                width: (ITEM_WIDTH - 10 * numberOfColumn) / numberOfColumn
+                            }}
+                                source={{ uri: item.url_s }}
+                                resizeMode="contain"
+                            />
+                        </View>
+                    )}
+                    keyExtractor={(item) => `${item.id}_${item.title}_${item.secret}`}
+                    numColumns={numberOfColumn}
+                    onEndReached={() => setPageNo(pageNo + 1)}
+                    onEndReachedThreshold={0.3}
+                    key={numberOfColumn}
+                    onRefresh={refreshPage}
+                    refreshing={refreshing}
                 />
-                </View>
-            )}
-            keyExtractor={(item) => `${item.id}_${item.title}_${item.secret}`}
-            numColumns={numberOfColumn}
-            onEndReached={() => setPageNo(pageNo+1)}
-            onEndReachedThreshold={0.3}
-            key={numberOfColumn}
-            onRefresh={refreshPage}
-            refreshing={refreshing}
-            />
-        </View>
-        
-        <TouchableOpacity 
-            onPress={changeColumn} 
-            style={styles.resizeView}
-        >
-            <Entypo
-                name="resize-full-screen"
-                color={"#FFF"}
-                size={20}
-            />
-        </TouchableOpacity>
-    </SafeAreaView>
-  );
+            </View>
+
+            <TouchableOpacity
+                onPress={changeColumn}
+                style={styles.resizeView}
+            >
+                <Entypo
+                    name="resize-full-screen"
+                    color={"#FFF"}
+                    size={20}
+                />
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-    safeAreaViewStyle:{
-        flex:1,
+    safeAreaViewStyle: {
+        flex: 1,
         backgroundColor: '#F7882F'
     },
     headerView: {
         height: 90,
         backgroundColor: '#F7882F',
-        flex:0,
-        justifyContent:'flex-end',
-      },
+        flex: 0,
+        justifyContent: 'flex-end',
+    },
     searchView: {
         backgroundColor: '#fff',
         height: 50,
@@ -171,25 +175,25 @@ const styles = StyleSheet.create({
         flex: 1,
         flexDirection: 'row',
         alignItems: 'center',
-      },
+    },
     iconStyle: {
-        margin:10,
+        margin: 10,
     },
     textInputStyle: {
         fontSize: 16,
         fontWeight: 'bold',
         flex: 1,
-      },
+    },
     resizeView: {
-        height:50,
-        width:50,
-        borderRadius:90,
-        backgroundColor:'#F7882F',
+        height: 50,
+        width: 50,
+        borderRadius: 90,
+        backgroundColor: '#F7882F',
         position: 'absolute',
         bottom: 40,
         right: 40,
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center'
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
     },
 });
